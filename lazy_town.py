733b5e90d7e0429affed135ab0f322d26e3335f4
luffier <ps3skywolf@gmail.com>
2016-03-12 15:02:32 +0000

    Added updater() to lt

--- lazy_town.py
+++ lazy_town.py
@@ -3,22 +3,66 @@ the result will be a mixture of English and the target language. Levenshtein
 algorithm is used (if you don't have python-Levenshtein, difflib will be used,
 and its results tend to be very different). You can change the default
 threshold of 1 (lower similarity ratio). At a value of 1, it would only copie
-lines that haven't changed. You could use  a slightly lower value to retain
+lines that haven't changed. You could use a slightly lower value to retain
 lines with negligible changes. Creates a log of missing files and lines."""
 import os, sys, copy, shutil, argparse
 from os.path import join, isdir, abspath, splitext, basename
 import common
 from lang import Lang
 try:
-    import Levenshtein
-    def ratio(string1, string2):
-        return Levenshtein.ratio(string1, string2)
+    from Levenshtein import ratio
 except ImportError:
     import difflib
     def ratio(string1, string2):
         return difflib.SequenceMatcher(None, string1, string2).ratio()
     print("** python-Levenshtein module not found, using difflib instead **")
 
+#updates target's files to match the file structure and naming on Fixt
+#(files were merged and some filenames were changed)
+def updater(base, base2, target, thd):
+
+    renamed_lib = {'assblow':['kalnor'],   'bosasis':['bosasist'],
+                   'boslori':['boslorri'], 'bvlad':  ['bv2vault'],
+                   'lorri':  ['lorraine'], 'blade':  ['inblade'],
+                   'trait':    ['trait - original descriptions',
+                                'trait - sduibek detailed descriptions'],
+                   'pro_misc': ['pro_misc - original exit grids',
+                                'pro_misc - sduibek exit grids'],
+                   'perk':     ['perk - original addiction titles',
+                                'perk - sduibek addictions titles'],
+                   'editor':   ['editor - original addiction titles',
+                                'editor - sduibek addiction titles']}
+
+    for lang in (base, target):
+        for filename, filenames_new in renamed_lib.items():
+            for filename_new in filenames_new:
+                if (filename in lang) and (not filename_new in lang):
+                    lang[filename_new] = lang[filename]
+                    lang.filepaths[filename_new] = None
+            lang.pop(filename, None)
+            lang.filepaths.pop(filename, None)
+
+
+
+    merged_lib = {'cow':'brahmin', 'demodog': 'alldogs', 'dog2': 'alldogs',
+                  'dogmeat': 'alldogs', 'junkdog': 'alldogs',
+                  'genlock':'locker', 'hhooker': 'hhookera'}
+
+    #incase the files already exist and to make space for the changes
+    for filename, filename_new in list(merged_lib.items()):
+        if (filename not in base) or (filename_new in base):
+            merged_lib.pop(filename)
+        base.purge_with(target, limiter=filename)
+
+    #compares the lines of the files listed in moved_lib
+    for filename, filename_new in merged_lib.items():
+        base.setdefault(filename_new, {})
+        target.setdefault(filename_new, {})
+        for index, line in list(base[filename].items()):
+            for index_new, line_new in list(base2[filename_new].items()):
+                if ratio(line, line_new) >= thd:
+                    base[filename_new][index_new] = line
+                    target[filename_new][index_new] = target[filename][index]
 
 #base: English files used during the localization process of target
 #base2: current English files
@@ -32,11 +76,13 @@ def comparator(base, base2, target, thd=1):
     target.purge_with(base) #in case the localization is incomplete
     base.purge_with(base2, target) #to satisfy the above
 
+    target2.flags = 0
     for filename in base:
         for index in base[filename]:
             if ratio(base[filename][index], base2[filename][index]) >= thd:
                 if target[filename][index]: #in case the line is empty
                     target2[filename][index] = target[filename][index]
+                    target2.flags += 1
     return target2
 
 #copies lang's content into the .MSG files inside dirpath
@@ -53,8 +99,8 @@ def injector(lang, dirpath, base_encoding):
                 line_sections = Lang.line_pattern.search(line)
                 index = int(line_sections.group(1))
                 if (index in lang[filename]) and line_sections.group(2):
-                    line = (line[:line_sections.start(2)]   +
-                            str(lang[filename][index])  +
+                    line = (line[:line_sections.start(2)] +
+                            str(lang[filename][index])    +
                             line[line_sections.end(2):])
                 text_out += line
             else:
@@ -77,7 +123,7 @@ if __name__ == '__main__':
                      help="Clears json cache files")
     args = par.parse_args()
 
-    target2_path =  join('.', basename(args.target) + '_NEW')
+    target2_path =  join('.', basename(args.target) + '_LAZY_UPDATE')
     if isdir(target2_path):
         shutil.rmtree(target2_path)
 
@@ -88,18 +134,44 @@ if __name__ == '__main__':
     base2 = Lang(abspath(args.base2), args.clearcache)
     target = Lang(abspath(args.target), args.clearcache)
 
+    t_size = target.size(False)
+    b2_size = base2.size(False)
+
+    updater(base, base2, target, args.threshold)
+
     shutil.copytree(args.base2, target2_path)
     target2 = comparator(base, base2, target, args.threshold)
+    flags = target2.flags
 
-    with open('lt-log.txt', 'w') as flog:
-        log = "MISSING FILES AND INDICES LOG"
-        for lang in (base2, target):
-            for lang_name in lang.purged:
+    with open('lt-log-{}.txt'.format(target.dirname), 'w') as flog:
+        log = "*"*31 + "\n"
+        log += "{:^31}\n".format("GENERAL LOG")
+        log += "*"*31 + "\n\n"
+
+        log += "* {} version: \n".format(target.dirname.capitalize())
+        log += "* English base version:\n"
+        log += "* English Fixt version:\n\n"
+
+        log += "* Similarity ratio threshold: {:.2f}\n\n".format(args.threshold)
+
+        log += "* Comments:\n\n"
+
+        log += "* {} of {} ({}%) {} lines were used.\n".format(
+        flags, t_size, int(flags/t_size * 100), target.dirname.capitalize())
+        log += "* {} of {} ({}%) lines remain in English.".format(
+        b2_size-flags, b2_size, int((b2_size-flags)/b2_size * 100))
+
+        log += "\n\n\n" + "*"*31 + "\n"
+        log += "{:^31}\n".format("MISSING FILES AND INDICES LOG")
+        log += "*"*31
+
+        for lang in (base, base2, target):
+            for lang_name in sorted(lang.purged):
                 log += "\n\n\nPresent in {}".format(lang_name)
                 log += " but not in {}\n".format(lang.dirname)
-                for filename in lang.purged[lang_name]:
+                for filename in sorted(lang.purged[lang_name]):
                     log += "\n{:<12.12} | ".format(filename)
-                    for index in lang.purged[lang_name][filename]:
+                    for index in sorted(lang.purged[lang_name][filename]):
                         log += " {}".format(index)
         flog.write(log)
 
